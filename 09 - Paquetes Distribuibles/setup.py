1c7e1aa61722fddf9c0e1dfc23afb1913e965d1f
Javier <javalejan12@gmail.com>
2023-05-16 22:27:07 -0500

    paquetes distribuidos

---
+++ 09 - Paquetes Distribuibles/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = "paquetecalculos",
+	version="1.0",
+	description="Paquete de Redondeo y Potencia",
+	author="Camilo",
+	author_email="andrescamilo468@gmail.com",
+	url="www.hola.com",
+	packages=["Calculos","Calculos.RedondeoPotencia"]
+	)
